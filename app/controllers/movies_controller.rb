7bee3a5953e9b72128a0bc3e3d5abf706509ecac
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-06-24 12:41:24 -0700

    Validation Messages and Error Handling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,8 +7,10 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = Movie.create(movie_params)
+    movie = Movie.create!(movie_params)
     render json: movie, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   private
